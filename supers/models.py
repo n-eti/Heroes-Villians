6866e21248a63c1005abfc2e850943627b839086
Nada Atieh <nada.s.atieh@gmail.com>
2023-01-11 13:29:57 -0600

    Updated supers and super_types model files + Registered rest framework in installed apps + migrated models into My SQL WB

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from super_types.models import Super_Types
 
 # Create your models here.
+
+class Super(models.Model):
+
+    name = models.CharField
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ability = models.CharField(max_length=255)
+    catchphrase = models.CharField(max_length=255)
+    super_type = models.ForeignKey(Super_Types, on_delete=models.CASCADE)
