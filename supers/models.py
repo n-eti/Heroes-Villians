74321e3381a8fee8b347b6dcb454a80c1f46ee06
Nada Atieh <nada.s.atieh@gmail.com>
2023-01-12 03:22:07 -0600

    Updated supers views file + Corrected names field in supers models folder with a maxlength integer so it would show up in the API.

--- supers/models.py
+++ supers/models.py
@@ -5,7 +5,7 @@ from super_types.models import Super_Types
 
 class Super(models.Model):
 
-    name = models.CharField
+    name = models.CharField(max_length=255)
     alter_ego = models.CharField(max_length=255)
     primary_ability = models.CharField(max_length=255)
     secondary_ability = models.CharField(max_length=255)
--- supers/views.py
+++ supers/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Super
+from .serializers import SuperSerializer
+
 @api_view(['GET'])
 def supers_list(request):
+    supers = Super.objects.all()
 
+    serializer = SuperSerializer(supers, many=True)
 
-    return Response('ok')
+    return Response(serializer.data)
